f1e9477200e31100283b118b2fd05964671431d1
Julian Orth <julian.orth@icloud.com>
2024-02-08 21:38:22 +0100

    On Country click zoom in and show country states on hover

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,6 +9,8 @@ import { IComponentDataItem } from "@amcharts/amcharts5/.internal/core/render/Co
 import styles from "../styles/Map.module.scss";
 import { useAppSelector } from "../hooks/hooks";
 import { loadUserDataFromFb } from "../services/firebaseHelper";
+import am5geodata_data_countries2 from "@amcharts/amcharts5-geodata/data/countries2";
+import { Button } from "@mui/material";
 
 type CountryCode = string;
 
@@ -37,34 +39,36 @@ export const Map = () => {
     const chart = root.container.children.push(
       am5map.MapChart.new(root, {
         panX: "rotateX",
-        projection: am5map.geoEqualEarth(),
+        projection: am5map.geoMercator(),
       })
     );
 
     chart.set("zoomControl", am5map.ZoomControl.new(root, {}));
 
     // Create polygon series
-    const polygonSeries = chart.series.push(
+    const worldSeries = chart.series.push(
       am5map.MapPolygonSeries.new(root, {
         geoJSON: am5geodata_worldLow,
-        exclude: ["AQ"],
+        exclude: [""],
       })
     );
 
-    polygonSeries.mapPolygons.template.setAll({
+    worldSeries.mapPolygons.template.setAll({
       tooltipText: "{name}",
       interactive: true,
       fill: am5.color(0xaaaaaa),
       templateField: "polygonSettings",
+      stroke: am5.color("#ffffff"),
+      strokeWidth: 1,
     });
 
-    polygonSeries.mapPolygons.template.states.create("hover", {
+    worldSeries.mapPolygons.template.states.create("hover", {
       //   fill: am5.color(0x677935),
       fill: colors.getIndex(13),
     });
 
-    polygonSeries.events.on("datavalidated", function () {
-      polygonSeries.mapPolygons.each(function (polygon) {
+    worldSeries.events.on("datavalidated", function () {
+      worldSeries.mapPolygons.each(function (polygon) {
         if (
           polygon.dataItem &&
           polygon.dataItem.dataContext !== null &&
@@ -79,23 +83,22 @@ export const Map = () => {
       });
     });
 
-    polygonSeries.mapPolygons.template.events.on(
+    worldSeries.mapPolygons.template.events.on(
       "click",
       (ev: am5.ISpritePointerEvent) => {
         const dataItem: DataItem<IComponentDataItem> | undefined =
           ev.target.dataItem;
         const data: any = dataItem?.dataContext;
         if (dataItem) {
-          const zoomAnimation = polygonSeries.zoomToDataItem(
+          const zoomAnimation = worldSeries.zoomToDataItem(
             dataItem as DataItem<IMapPolygonSeriesDataItem>
           );
-
+          console.log(dataItem);
           Promise.all([
             zoomAnimation?.waitForStop(),
             am5.net.load(
               "https://cdn.amcharts.com/lib/5/geodata/json/" +
-                // data.map +
-                "russia" +
+                data.map +
                 ".json",
               chart
             ),
@@ -107,15 +110,21 @@ export const Map = () => {
               ]
             ) => {
               if (results[1].response) {
+                console.log(results);
                 var geodata = am5.JSONParser.parse(results[1].response);
-                countrySeries.setAll({
-                  geoJSON: geodata,
-                  fill: data.polygonSettings.fill,
-                });
+                if (data && data.polygonSettings) {
+                  countrySeries.setAll({
+                    geoJSON: geodata,
+                    fill: data.polygonSettings.fill,
+                  });
+                } else {
+                  // Handle the case where data or data.polygonSettings is undefined
+                  // You might want to set a default fill color or handle the error appropriately
+                }
 
                 countrySeries.show();
-                polygonSeries.hide(100);
-                backContainer.show();
+                worldSeries.hide(100);
+                backButton.show();
               }
             }
           );
@@ -139,50 +148,90 @@ export const Map = () => {
       fill: colors.getIndex(9),
     });
 
-    // Add button to go back to continents view
-    var backContainer = chart.children.push(
-      am5.Container.new(root, {
-        x: am5.p100,
-        centerX: am5.p100,
-        dx: -10,
-        paddingTop: 5,
-        paddingRight: 10,
-        paddingBottom: 5,
-        y: 30,
-        interactiveChildren: false,
-        layout: root.horizontalLayout,
-        cursorOverStyle: "pointer",
-        background: am5.RoundedRectangle.new(root, {
-          fill: am5.color(0xffffff),
-          fillOpacity: 0.2,
+    var continents: { [key: string]: number } = {
+      AF: 0,
+      AN: 1,
+      AS: 2,
+      EU: 3,
+      NA: 4,
+      OC: 5,
+      SA: 6,
+    };
+
+    // Set up data for countries
+    var data = [];
+    for (var id in am5geodata_data_countries2) {
+      if (am5geodata_data_countries2.hasOwnProperty(id)) {
+        var country = am5geodata_data_countries2[id];
+        if (country.maps.length) {
+          data.push({
+            id: id,
+            map: country.maps[0],
+            polygonSettings: {
+              fill: colors.getIndex(continents[country.continent_code]),
+            },
+          });
+        }
+      }
+    }
+    worldSeries.data.setAll(data);
+
+    // Add custom button
+    const backButton = chart.children.push(
+      am5.Button.new(root, {
+        x: am5.percent(70),
+        dy: 10,
+        label: am5.Label.new(root, {
+          text: "Back To World Map",
         }),
         visible: false,
       })
     );
 
-    var backLabel = backContainer.children.push(
-      am5.Label.new(root, {
-        text: "Back to world map",
-        centerY: am5.p50,
-      })
-    );
-
-    var backButton = backContainer.children.push(
-      am5.Graphics.new(root, {
-        width: 32,
-        height: 32,
-        centerY: am5.p50,
-        fill: am5.color(0x555555),
-        svgPath: "",
-      })
-    );
-
-    backContainer.events.on("click", function () {
+    backButton.events.on("click", function () {
       chart.goHome();
-      polygonSeries.show();
+      worldSeries.show();
       countrySeries.hide();
-      backContainer.hide();
+      backButton.hide();
     });
+
+    // // Add button to go back to continents view
+    // var backContainer = chart.children.push(
+    //   am5.Container.new(root, {
+    //     x: am5.p50,
+    //     centerX: am5.p50,
+    //     dx: -10,
+    //     paddingTop: 5,
+    //     paddingRight: 10,
+    //     paddingBottom: 5,
+    //     y: 30,
+    //     interactiveChildren: false,
+    //     layout: root.horizontalLayout,
+    //     cursorOverStyle: "pointer",
+    //     background: am5.RoundedRectangle.new(root, {
+    //       fill: am5.color(0xffffff),
+    //       fillOpacity: 0.2,
+    //     }),
+    //     visible: true,
+    //   })
+    // );
+
+    // var backLabel = backContainer.children.push(
+    //   am5.Label.new(root, {
+    //     text: "Back to world map",
+    //     centerY: am5.p50,
+    //   })
+    // );
+
+    // var backButton = backContainer.children.push(
+    //   am5.Graphics.new(root, {
+    //     width: 32,
+    //     height: 32,
+    //     centerY: am5.p50,
+    //     fill: am5.color(0x555555),
+    //     svgPath: "",
+    //   })
+    // );
 
     return () => root.dispose();
   }, [userData]);
