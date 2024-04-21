92dd36a1a18375747aff4682906a0d6805e9c44a
Julian Orth <julian.orth@icloud.com>
2024-04-21 10:08:04 +0200

    Login moved to custom hook

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,44 +1,10 @@
-import {
-  GoogleAuthProvider,
-  getAdditionalUserInfo,
-  getAuth,
-  signInWithPopup,
-} from "firebase/auth";
 import { Button } from "@mui/material";
-import { useAppDispatch } from "../hooks/hooks";
-import { setUserLoggedIn } from "../store/userSlice";
+import { useGoogleLogin } from "../hooks/useGoogleLogin";
 
 export const Login = () => {
-  const handleLogin = async () => {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    const dispatch = useAppDispatch();
+  const { loginWithGoogle } = useGoogleLogin();
 
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-
-      const isNewUser = getAdditionalUserInfo(result);
-      console.log(isNewUser);
-      if (isNewUser) {
-        console.log("Erfolgreich NEU angemeldet:", user.displayName);
-        // Perform actions specific to first-time sign-up
-      } else {
-        console.log("Erfolgreich nicht neu angemeldet:", user.displayName);
-        // Perform actions specific to login
-      }
-
-      dispatch(setUserLoggedIn(true));
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error("Anmeldefehler:", error.message);
-      } else {
-        console.error("An unhandled error occurred:", error);
-      }
-    }
-  };
-
-  return <Button onClick={handleLogin}>Mit Google anmelden</Button>;
+  return <Button onClick={loginWithGoogle}>Sign in with Google</Button>;
 };
 
 export default Login;
---
+++ src/hooks/useGoogleLogin.tsx
@@ -0,0 +1,42 @@
+import { useAppDispatch } from "./hooks";
+import { setUserLoggedIn } from "../store/userSlice";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  getAdditionalUserInfo,
+} from "firebase/auth";
+
+// Login with Google Auth Provider, dispatch is logged in to redux store
+export const useGoogleLogin = () => {
+  const dispatch = useAppDispatch();
+
+  const loginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const isNewUser = getAdditionalUserInfo(result);
+
+      if (isNewUser) {
+        console.log("Successfully loggid in:", user.displayName);
+        // Perform actions specific to first-time sign-up
+      } else {
+        console.log("Successfully loggid in (first time):", user.displayName);
+        // Perform actions specific to login
+      }
+
+      dispatch(setUserLoggedIn(true));
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error logging in:", error.message);
+      } else {
+        console.error("An unhandled error occurred:", error);
+      }
+    }
+  };
+
+  return { loginWithGoogle };
+};
