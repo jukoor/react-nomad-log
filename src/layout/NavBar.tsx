51f50636f19e37a75659c1ad7a7a901ddb233921
Julian Orth <julian.orth@icloud.com>
2024-04-27 09:15:37 +0200

    Navbar clean up

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -5,33 +5,18 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import styles from "../styles/NavBar.module.scss";
-import {
-  Avatar,
-  Button,
-  LinearProgress,
-  Link,
-  ListItemIcon,
-  ListItemText,
-  Menu,
-  MenuItem,
-} from "@mui/material";
+import { LinearProgress, Link } from "@mui/material";
 import { toggleMenuVisibility } from "../store/appSlice";
 import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Login from "../components/Login";
-import { getFirstLettersFromName } from "../utils/appUtils";
-import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 
-import ToggleOnOutlinedIcon from "@mui/icons-material/ToggleOnOutlined";
-import LogoutIcon from "@mui/icons-material/Logout";
 import { AvatarMenu } from "./AvatarMenu";
 
 export const NavBar = () => {
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
   const auth = getAuth();
 
@@ -40,39 +25,12 @@ export const NavBar = () => {
   const countryData = useAppSelector((state) => state.Country);
   const [loading, setLoading] = useState(false);
 
-  const [avatar, setAvatar] = useState<null | HTMLElement>(null);
-  const avatarMenuOpen = Boolean(avatar);
-
-  const handleAvatarClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(event.currentTarget);
-    setAvatar(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAvatar(null);
-  };
-
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
-
   useEffect(() => {
     setLoading(userData.loading || countryData.loading);
   }, [userData.loading, countryData.loading]);
 
   const handleOnClick = () => {
     dispatch(toggleMenuVisibility());
-  };
-
-  const handleMenuProfileClick = () => {
-    navigate("/profile/8pVS1cDjBszgEUE0aug8");
-  };
-
-  const handleMenuSettingseClick = () => {
-    navigate("/settings");
-  };
-
-  const handleMenuLogoutClick = () => {
-    navigate("/logout");
   };
 
   return (
