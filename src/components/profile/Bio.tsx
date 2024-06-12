6415fbbb6149ce0555dc7c4599796af10182995b
Julian Orth <julian.orth@icloud.com>
2024-06-12 18:42:21 +0200

    Refactored Bio Sceleton Loading, UI improvements

--- src/components/profile/Bio.tsx
+++ src/components/profile/Bio.tsx
@@ -10,13 +10,112 @@ import {
 } from "@mui/material";
 
 import styles from "../../styles/Bio.module.scss";
-import { randomColorStringAvatar } from "../../utils/appUtils";
 import { useAppSelector } from "../../hooks/reduxHooks";
-import React from "react";
+import { getEmojiFlag } from "../../utils/countryDataUtils";
 
 export const Bio = () => {
   const userData = useAppSelector((state) => state.User.selectedUser);
   const userDataLoading = useAppSelector((state) => state.User.loading);
+
+  {
+    /* Render sceleton during data loading */
+  }
+  const renderContent = () => {
+    switch (userDataLoading) {
+      case true:
+        return (
+          <>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "20px",
+                marginBottom: "25px",
+              }}
+            >
+              <Skeleton variant="circular" width={40} height={40} />
+              <Skeleton variant="rounded" height={35} width={"50%"} />
+            </Box>
+
+            <Skeleton variant="text" width={"30%"} sx={{ mb: 1.5 }} />
+            <Skeleton variant="text" width={"50px"} sx={{ mb: 1.5 }} />
+            <Skeleton variant="rounded" width={"100%"} height={180} />
+          </>
+        );
+      case false:
+        return (
+          <>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: "20px",
+                marginBottom: "25px",
+              }}
+            >
+              <Avatar sx={{ backgroundColor: "#cfcfcf" }}>
+                {getEmojiFlag(userData?.homeCountry)}
+              </Avatar>
+              <Typography
+                variant="h5"
+                component="h1"
+                color="text.secondary"
+                sx={{ color: "#212121" }}
+                className={styles.name}
+              >
+                {userData?.nameFirst} {userData?.nameLast}
+              </Typography>
+            </Box>
+
+            <div className={styles.tags}>
+              {userData?.tags?.map((item: string, index: number) => (
+                <Chip
+                  className={styles.tag}
+                  key={index}
+                  label={item}
+                  variant="outlined"
+                  size="small"
+                  color="primary"
+                  sx={{ mb: 1.5 }}
+                />
+              ))}
+            </div>
+
+            <Typography
+              variant="button"
+              display="block"
+              gutterBottom
+              sx={{ fontWeight: "bold" }}
+            >
+              Currently Living
+            </Typography>
+
+            <Divider sx={{ mb: "10px" }} />
+
+            <Typography variant="body2" sx={{ mb: 3 }}>
+              {userData?.livingInCity}
+            </Typography>
+
+            <Typography
+              variant="button"
+              display="block"
+              gutterBottom
+              sx={{ fontWeight: "bold" }}
+            >
+              BIO
+            </Typography>
+
+            <Divider sx={{ mb: "10px" }} />
+
+            <Typography variant="body2" className={styles.bio}>
+              {userData?.bio}
+            </Typography>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
     <Card
@@ -27,104 +126,7 @@ export const Bio = () => {
       }}
     >
       <CardContent sx={{ padding: { xs: "20px", sm: "20px", md: "30px" } }}>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            gap: "20px",
-            marginBottom: "35px",
-          }}
-        >
-          {userDataLoading ? (
-            <Skeleton variant="circular" width={40} height={40} />
-          ) : (
-            <Avatar
-              {...randomColorStringAvatar(
-                `${userData?.nameFirst} ${userData?.nameLast}`
-              )}
-            ></Avatar>
-          )}
-
-          {userDataLoading ? (
-            <Skeleton variant="rounded" height={35} width={"50%"} />
-          ) : (
-            <Typography
-              variant="h5"
-              component="h1"
-              color="text.secondary"
-              className={styles.name}
-            >
-              {userData?.nameFirst} {userData?.nameLast}
-            </Typography>
-          )}
-        </Box>
-
-        {userDataLoading ? (
-          <Skeleton variant="text" width={"30%"} />
-        ) : (
-          <Typography color="text.secondary" className={styles.nationality}>
-            Home Country: {userData?.homeCountry}
-          </Typography>
-        )}
-
-        {/* Tags */}
-        {userDataLoading ? (
-          <Skeleton variant="text" width={"30%"} sx={{ mb: 1.5 }} />
-        ) : (
-          <Typography
-            sx={{ mb: 1.5 }}
-            color="text.secondary"
-            className={styles.nationality}
-          >
-            Currently Living: {userData?.livingInCity}
-          </Typography>
-        )}
-
-        {/* Tags */}
-        <div className={styles.tags}>
-          {userData?.tags?.map((item: string, index: number) => {
-            return (
-              <React.Fragment key={index}>
-                {userDataLoading ? (
-                  <Skeleton
-                    key={index}
-                    variant="text"
-                    height={32}
-                    width={"100px"}
-                    sx={{ mr: 0.5 }}
-                  />
-                ) : (
-                  <Chip
-                    className={styles.tag}
-                    key={index}
-                    label={item}
-                    variant="outlined"
-                    color="primary"
-                    sx={{ mb: 1.5 }}
-                  />
-                )}
-              </React.Fragment>
-            );
-          })}
-        </div>
-
-        {/* Bio */}
-        {userDataLoading ? (
-          <Skeleton variant="text" width={"50px"} sx={{ mb: 1.5 }} />
-        ) : (
-          <Typography variant="button" display="block" gutterBottom>
-            BIO
-          </Typography>
-        )}
-
-        <Divider sx={{ mb: "10px" }} />
-        {userDataLoading ? (
-          <Skeleton variant="rounded" width={"100%"} height={180} />
-        ) : (
-          <Typography variant="body2" className={styles.bio}>
-            {userData?.bio}
-          </Typography>
-        )}
+        {renderContent()}
       </CardContent>
     </Card>
   );
--- src/utils/countryDataUtils.tsx
+++ src/utils/countryDataUtils.tsx
@@ -1,13 +1,22 @@
+import { CountryCca2Type } from "../types/CountryCca2Type";
+
 /* Returns Emoji flag icon by country code */
-export const getEmojiFlag = (countryCode: string) => {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt(0));
-  return String.fromCodePoint(...codePoints);
+export const getEmojiFlag = (
+  countryCode: CountryCca2Type | null | undefined
+) => {
+  if (countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split("")
+      .map((char) => 127397 + char.charCodeAt(0));
+    return String.fromCodePoint(...codePoints);
+  } else return "";
 };
 
 /* Searches all countries data to return the specific country data by countryCode input */
-export const getCountryData = (countryCode: string, allCountries: any[]) => {
+export const getCountryData = (
+  countryCode: CountryCca2Type,
+  allCountries: any[]
+) => {
   return allCountries?.find((country) => country.cca2 === countryCode);
 };
